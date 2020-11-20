f3a34209f3d2ccb58757893c5f66424a4052a1a2
Robson <robhenrod@gmail.com>
2020-11-20 15:36:52 -0300

    Nivel 2 - Iniciando back-end do app

---
+++ nivel-2/iniciando-back-end/src/models/Appointment.ts
@@ -0,0 +1,35 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+@Entity('appointments')
+class Appointment {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @Column()
+  provider_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
+
+  @Column('time with time zone')
+  date: Date;
+}
+
+export default Appointment;
---
+++ nivel-2/iniciando-back-end/src/routes/sessions.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRouter = Router();
+
+sessionsRouter.post('/', async (resquest, response) => {
+  const { email, password } = resquest.body;
+
+  const authenticateUser = new AuthenticateUserService();
+
+  const { user, token } = await authenticateUser.execute({
+    email,
+    password,
+  });
+
+  delete user.password;
+
+  return response.json({ user, token });
+});
+
+export default sessionsRouter;
