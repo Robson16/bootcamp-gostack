1370a8252dfef055bab0a61c503847abafaa1f8c
Robson <robhenrod@gmail.com>
2020-12-10 15:03:54 -0300

    SignIn Page

--- nivel-3/iniciando-front-end-web/src/styles/global.ts
+++ nivel-3/iniciando-front-end-web/src/styles/global.ts
@@ -4,6 +4,7 @@ export default createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
+    outline: 0;
     box-sizing: border-box;
   }
 
@@ -17,7 +18,7 @@ export default createGlobalStyle`
     font: 16px 'Roboto Slab', serif;
   }
 
-  h1, h2, h3, h4, h5,h6,strong {
+  h1, h2, h3, h4, h5, h6, strong {
     font-weight: 500
   }
 
