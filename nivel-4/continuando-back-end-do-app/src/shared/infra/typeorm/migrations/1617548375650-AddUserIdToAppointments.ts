95f21af13d065522209e51bafb9e02e46307fd13
Robson <robhenrod@gmail.com>
2021-04-05 22:35:46 -0300

    Fix migration down

--- nivel-4/continuando-back-end-do-app/src/shared/infra/typeorm/migrations/1617548375650-AddUserIdToAppointments.ts
+++ nivel-4/continuando-back-end-do-app/src/shared/infra/typeorm/migrations/1617548375650-AddUserIdToAppointments.ts
@@ -31,7 +31,7 @@ export default class AddUserIdToAppointments1617548375650
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropForeignKey('appointments', 'AppointmentProvider');
+    await queryRunner.dropForeignKey('appointments', 'AppointmentUser');
     await queryRunner.dropColumn('appointments', 'user_id');
   }
 }
