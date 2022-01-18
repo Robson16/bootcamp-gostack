b09fe37f9b0043faceee8eb89bcd8dd4353d8d6e
robsonhr16 <robhenrod@gmail.com>
2022-01-18 11:52:07 -0300

    Nivel 4 - Finalizando back-end do app ( Aula: Utilizando Class Transformer )

--- nivel-4/finalizando-back-end-do-app/src/modules/users/infra/typeorm/entities/User.ts
+++ nivel-4/finalizando-back-end-do-app/src/modules/users/infra/typeorm/entities/User.ts
@@ -6,6 +6,8 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+import { Exclude, Expose } from 'class-transformer';
+
 @Entity('users')
 class User {
   @PrimaryGeneratedColumn('uuid')
@@ -24,10 +26,18 @@ class User {
   email: string;
 
   @Column()
+  @Exclude()
   password: string;
 
   @Column()
   avatar: string;
+
+  @Expose({ name: 'avatar_url' })
+  getAvatarUrl(): string | null {
+    return this.avatar
+      ? `${process.env.APP_API_URL}/files/${this.avatar}`
+      : null;
+  }
 }
 
 export default User;
