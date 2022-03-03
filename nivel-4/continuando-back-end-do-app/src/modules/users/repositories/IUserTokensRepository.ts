c520bd74cbc18ea7df13365a157d8caa839ea8ff
robsonhr16 <robhenrod@gmail.com>
2022-03-03 20:58:04 -0300

    Simplificando a estrutura dos arquivos

--- nivel-4/continuando-back-end-do-app/src/modules/users/repositories/IUserTokensRepository.ts
+++
@@ -1,7 +0,0 @@
-import User from '../infra/typeorm/entities/User';
-import UserToken from '../infra/typeorm/entities/UserToken';
-
-export default interface IUserTokensRepository {
-  generate(user_id: string): Promise<UserToken>;
-  findByToken(token: string): Promise<UserToken | undefined>;
-}
