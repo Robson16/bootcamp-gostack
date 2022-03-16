25828dcf063018ab5b13482cddbfa6d84849b0be
robsonhr16 <robhenrod@gmail.com>
2022-03-15 22:03:37 -0300

    Nivel 5 - Finalizando front-end mobile do app ( Aula: Atualização dos dados )

--- gobarber/mobile/src/pages/Profile/index.tsx
+++ gobarber/mobile/src/pages/Profile/index.tsx
@@ -34,11 +34,13 @@ import {
 interface ProfileFormData {
   name: string;
   email: string;
+  old_password: string;
   password: string;
+  password_confirmation: string;
 }
 
 const Profile: React.FC = () => {
-  const { user } = useAuth();
+  const { user, updateUser } = useAuth();
 
   const formRef = useRef<FormHandles>(null);
   const navigation = useNavigation();
@@ -58,18 +60,52 @@ const Profile: React.FC = () => {
           email: Yup.string()
             .required('E-mail obrigatório')
             .email('Digite um e-mail válido'),
-          password: Yup.string().min(6, 'No minimo 6 digitos'),
+          old_password: Yup.string(),
+          password: Yup.string().when('old_password', {
+            is: val => !!val.length,
+            then: Yup.string().required('Campo obrigatório'),
+            otherwise: Yup.string(),
+          }),
+          password_confirmation: Yup.string()
+            .when('old_password', {
+              is: val => !!val.length,
+              then: Yup.string().required('Campo obrigatório'),
+              otherwise: Yup.string(),
+            })
+            .oneOf([Yup.ref('password'), undefined], 'Confirmação incorreta'),
         });
 
         await schema.validate(data, {
           abortEarly: false,
         });
 
-        await api.post('/users', data);
+        const {
+          name,
+          email,
+          old_password,
+          password,
+          password_confirmation,
+        } = data;
+
+        const updated_passwords = {
+          old_password,
+          password,
+          password_confirmation,
+        };
+
+        const formData = {
+          name,
+          email,
+          ...(old_password ? updated_passwords : {}),
+        };
+
+        const response = await api.put('/profile', formData);
+
+        updateUser(response.data);
 
         Alert.alert(
-          'Cadastro realizado com sucesso!',
-          'Você já pode fazer logon na aplicação.',
+          'Perfil atualizado!',
+          'Suas informações do perfil foram atualizadas com sucesso!',
         );
 
         navigation.goBack();
@@ -83,8 +119,8 @@ const Profile: React.FC = () => {
         }
 
         Alert.alert(
-          'Erro no cadastro',
-          'Ocorreu um erro ao fazer cadastro, tente novamente.',
+          'Erro na atualização',
+          'Ocorreu um erro ao atualizar perfil, tente novamente.',
         );
       }
     },
@@ -123,7 +159,7 @@ const Profile: React.FC = () => {
               <Title>Meu perfil</Title>
             </View>
 
-            <Form ref={formRef} onSubmit={handleProfile}>
+            <Form initialData={user} ref={formRef} onSubmit={handleProfile}>
               <Input
                 autoCapitalize="words"
                 name="name"
